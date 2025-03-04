ed41f0fa41dc6e4d823bd41f6d5318d632ab8bf9
Garima <garimaawasthi11220@gmail.com>
2025-03-04 12:42:33 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import FormPage from "./components/Formpage";
 import MapPage from "./components/Mappage";
+
+const NotFoundPage = () => {
+  return <h2>404: Page Not Found</h2>;
+};
 
 const App = () => {
   const [userData, setUserData] = useState(null);
 
   return (
-    <Router>
+    <Router basename="/ottermap-project">
       <Routes>
-        <Route path="/ottermap-project" element={<FormPage setUserData={setUserData} />} />
+        <Route path="/" element={<FormPage setUserData={setUserData} />} />
         <Route path="/map" element={<MapPage userData={userData} />} />
+        {/* Redirect unknown routes to FormPage */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
